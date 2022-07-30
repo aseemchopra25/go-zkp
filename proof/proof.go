82cd16c1881f83fe4e579ea036fce94c2f2a0d87
aseemchopra25 <aseemchopra@protonmail.com>
2022-07-30 08:22:35 +0530

    :sparkles: Verification Successful

--- proof/proof.go
+++ proof/proof.go
@@ -31,7 +31,7 @@ func CreateProof() {
 	db.DB.Random = db.DB.RandomKey.PublicKey.X.Bytes()
 	db.DB.Pubkey = db.DB.Key.PublicKey.X.Bytes()
 
-	// Challenge
+	// Challenge: This seems to be optional/mostly signature related
 	cbytes := sha256.Sum256(concat(db.DB.Curve.Params().Gx.Bytes(), db.DB.Random, db.DB.Pubkey))
 	db.DB.Challenge.SetBytes((cbytes[:]))
 
@@ -45,10 +45,31 @@ func CreateProof() {
 
 func VerifyProof() {
 	// Check if X and Y coordinates of Public key given lie on Curve P256
-	if !db.DB.Curve.IsOnCurve(db.DB.Key.X, db.DB.Key.Y) {
-		fmt.Println("Verification Failed!")
+	if db.DB.Curve.IsOnCurve(db.DB.Key.X, db.DB.Key.Y) {
+		fmt.Println("Public Key on Curve!")
 	} else {
-		fmt.Println("Pass!")
+		fmt.Println("Public Key not on the Curve!")
+	}
+
+	// Recreate challenge
+
+	cbytes := sha256.Sum256(concat(db.DB.Curve.Params().Gx.Bytes(), db.DB.Random, db.DB.Pubkey))
+	c := cbytes[:]
+
+	grx, gry := db.DB.Curve.ScalarBaseMult(db.DB.Response.Bytes())
+
+	acx, acy := db.DB.Curve.ScalarMult(db.DB.Key.X, db.DB.Key.Y, c)
+	fmt.Println("Received from Prover: ")
+	t1 := db.DB.RandomKey.PublicKey.X
+	fmt.Println(t1)
+	fmt.Println("Verification Output:")
+	t2, _ := db.DB.Curve.Add(grx, gry, acx, acy)
+	fmt.Println(t2)
+	fmt.Println("")
+	if t2.Cmp(t1) == 0 {
+		fmt.Println("Verification Successful")
+	} else {
+		fmt.Println("Verification Failed")
 	}
 
 }
